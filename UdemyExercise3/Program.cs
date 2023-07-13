31c19c9194583cf9965f26ceeebfc28315e07054
Michael Driscoll <michael.driscoll951@gmail.com>
2023-07-12 20:58:43 -0700

    exercise3

---
+++ UdemyExercise3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace UdemyExercise3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte myByte = 25;
+        Console.WriteLine(myByte);
+        sbyte mySbyte = -15;
+        Console.WriteLine(mySbyte);
+        int myInt = 12345678;
+        Console.WriteLine(myInt);
+        uint myUint = 123456;
+        Console.WriteLine(myUint);
+        short myShort = 323;
+        Console.WriteLine(myShort);
+        ushort myUshort = 2345;
+        Console.WriteLine(myUshort);
+        float myFloat = 32.15f;
+        Console.WriteLine(myFloat);
+        double myDouble = 3.14;
+        Console.WriteLine(myDouble);
+        char myLetter = 'a';
+        Console.WriteLine(myLetter);
+        bool myBool = true;
+        Console.WriteLine(myBool);
+        string myText = "my text";
+        Console.WriteLine(myText);
+        string numText = "15";
+        Console.WriteLine(numText);
+
+        int numFromText = int.Parse(numText);
+        Console.WriteLine(numFromText);
+        Console.ReadLine();
+
+
+    }
+}
+
